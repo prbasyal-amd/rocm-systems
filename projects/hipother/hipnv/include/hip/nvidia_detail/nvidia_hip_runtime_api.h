c7168aa1385d14ae25de1a8bfd4682059b64d39d
Satyanvesh Dittakavi <53337087+satyanveshd@users.noreply.github.com>
2025-08-21 18:35:40 +0530

    SWDEV-551218 - Fix hip on nvidia build failures

--- projects/hipother/hipnv/include/hip/nvidia_detail/nvidia_hip_runtime_api.h
+++ projects/hipother/hipnv/include/hip/nvidia_detail/nvidia_hip_runtime_api.h
@@ -2060,11 +2060,11 @@ inline static hipError_t hipMemPrefetchAsync_v2(const void* dev_ptr, size_t coun
 inline static hipError_t hipMemAdvise_v2(const void* dev_ptr, size_t count, hipMemoryAdvise advice,
                                          hipMemLocation location) {
 #if CUDA_VERSION >= 13000
-    return hipCUDAErrorTohipError(cudaMemAdvise(dev_ptr, count,
-        hipMemoryAdviseTocudaMemoryAdvise(advice), location));
+  return hipCUDAErrorTohipError(cudaMemAdvise(dev_ptr, count,
+      hipMemoryAdviseTocudaMemoryAdvise(advice), location));
 #else
-    return hipCUDAErrorTohipError(cudaMemAdvise_v2(dev_ptr, count,
-        hipMemoryAdviseTocudaMemoryAdvise(advice), location));
+  return hipCUDAErrorTohipError(cudaMemAdvise_v2(dev_ptr, count,
+      hipMemoryAdviseTocudaMemoryAdvise(advice), location));
 #endif
 }
 
@@ -2171,7 +2171,7 @@ inline static hipError_t hipSetDevice(int device) {
   return hipCUDAErrorTohipError(cudaSetDevice(device));
 }
 
-/inline static hipError_t hipChooseDevice(int* device, const hipDeviceProp_t* prop) {
+inline static hipError_t hipChooseDevice(int* device, const hipDeviceProp_t* prop) {
   if (prop == NULL) {
     return hipErrorInvalidValue;
   }
